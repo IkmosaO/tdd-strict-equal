a8ec817a043c22d64598af1dfab341af2d164aba
Ikponmwosa <otasowieik@gmail.com>
2021-10-31 21:38:11 -0400

    added files

--- tests/function.test.js
+++ tests/function.test.js
@@ -6,74 +6,56 @@ const it = require('mocha').it
 
 describe('strictlyEqual', () => {
   it('return true if values are strictlyequal', () => {
-    // set up test data
     const firstValue = '5'
     const secondValue = '5'
 
-    // call the function with the test data
     const result = strictlyEqual(firstValue, secondValue)
 
-    // make assertions about what should be true
     expect(result).to.be.true
   })
 
   it('return true if values are strictlyequal', () => {
-    // set up test data
     const firstValue = 'this is a string'
     const secondValue = 'this is a string'
 
-    // call the function with the test data
     const result = strictlyEqual(firstValue, secondValue)
 
-    // make assertions about what should be true
     expect(result).to.be.true
   })
 
   it('return true if values are not strictlyequal', () => {
-    // set up test data
     const firstValue = 5
     const secondValue = 5
 
-    // call the function with the test data
     const result = strictlyEqual(firstValue, secondValue)
 
-    // make assertions about what should be true
     expect(result).to.be.true
   })
 
   it('return false if values are not strictlyequal', () => {
-    // set up test data
     const firstValue = 'this is a string'
     const secondValue = 'this is not a string'
 
-    // call the function with the test data
     const result = strictlyEqual(firstValue, secondValue)
 
-    // make assertions about what should be false
     expect(result).to.be.false
   })
 
   it('return false if values are not strictlyequal', () => {
-    // set up test data
     const firstValue = 'this is a string'
     const secondValue = '5'
 
-    // call the function with the test data
     const result = strictlyEqual(firstValue, secondValue)
 
-    // make assertions about what should be false
     expect(result).to.be.false
   })
 
   it('return false if values are not strictlyequal', () => {
-    // set up test data
     const firstValue = '10'
     const secondValue = '6'
 
-    // call the function with the test data
     const result = strictlyEqual(firstValue, secondValue)
 
-    // make assertions about what should be false
     expect(result).to.be.false
   })
 })
